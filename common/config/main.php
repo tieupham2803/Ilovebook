da58bc1fc7441c27169f9e9ddc9aba1983a2389b
tieupham2803 <tieupham2803@gmail.com>
2017-03-09 22:37:09 +0700

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+        'class' => '\yii\db\Connection',
+        'dsn' => 'mysql:host=SQL5.FREEMYSQLHOSTING.NET;dbname=sql9162918',
+        'username' => 'sql9162918',
+        'password' => 'mC9WPAgU67',
+        'charset' => 'utf8',
+    ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
